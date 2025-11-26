80aa28678f5cbbd91e4caf10a8d97adcf4b78ee2
safdar ali khan <50498616+safdaralikhan@users.noreply.github.com>
2025-11-27 04:21:36 +0500

    Make userId and guestId optional in Cart model

--- models/Cart.js
+++ models/Cart.js
@@ -5,15 +5,20 @@ const cartSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
-     guestId : String,
+     guestId: {
+    type: String,
+    required: false,
+  },
     products: [
       {
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true, },
         qty: { type: Number, default: 1 },
       }
-    ]
+    ],
+    
+
   },
   { timestamps: true }
 );
